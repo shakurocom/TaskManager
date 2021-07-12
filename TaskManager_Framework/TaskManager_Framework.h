7311622afd9e2fa508f1e13d418dcc8f6c9c3cee
Sergey Laschuk <slaschuk@shakuro.com>
2021-07-12 11:49:03 +0300

    project for framework to handle sources

---
+++ TaskManager_Framework/TaskManager_Framework.h
@@ -0,0 +1,18 @@
+//
+//  TaskManager_Framework.h
+//  TaskManager_Framework
+//
+//  Created by Stepanov Konstantin on 12.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TaskManager_Framework.
+FOUNDATION_EXPORT double TaskManager_FrameworkVersionNumber;
+
+//! Project version string for TaskManager_Framework.
+FOUNDATION_EXPORT const unsigned char TaskManager_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaskManager_Framework/PublicHeader.h>
+
+
